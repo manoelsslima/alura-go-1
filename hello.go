18e95db06fa27fc07514d440f7abfd8adabd801a
Manoel Lima <manoelsslima@yahoo.com.br>
2025-01-17 00:51:06 -0400

    feat: function, package os, Exit

--- hello.go
+++ hello.go
@@ -2,16 +2,15 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Informe a opção desejada")
-	fmt.Println("1 - Iniciar")
-	fmt.Println("2 - Executar ação")
-	fmt.Println("9 - Sair da aplicação")
-	var opcao int
-	fmt.Scanf("%d", &opcao)
-	fmt.Println("A opção escolhida foi", opcao)
+
+	exibeIntroducao()
+	exibeMenu()
+	// var opcao = capturaOpcaoMenu()
+	opcao := capturaOpcaoMenu()
 
 	switch opcao {
 	case 1:
@@ -20,7 +19,28 @@ func main() {
 		fmt.Println("Executando ação...")
 	case 9:
 		fmt.Println("Saindo...")
+		os.Exit(0)
 	default:
 		fmt.Println("Opção inválida!")
+		os.Exit(-1)
 	}
+}
+
+func exibeIntroducao() {
+	fmt.Println("Bem-vindo ao sistema!")
+	fmt.Println("")
+}
+
+func exibeMenu() {
+	fmt.Println("Informe a opção desejada")
+	fmt.Println("1 - Iniciar")
+	fmt.Println("2 - Executar ação")
+	fmt.Println("9 - Sair da aplicação")
+}
+
+func capturaOpcaoMenu() int {
+	var opcao int
+	fmt.Scanf("%d", &opcao)
+	fmt.Println("A opção escolhida foi", opcao)
+	return opcao
 }
